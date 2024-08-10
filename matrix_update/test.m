bc4a8ece644ccef83b8e031295a06921a62b3da9
fahmiee <fahmidafida7@example.com>
2024-08-10 14:09:11 +0530

    optimizing srskelf

--- matrix_update/test.m
+++ matrix_update/test.m
@@ -40,7 +40,7 @@ function all_equal = compare_specific_fields(factors_original, factors_modified)
 end
 
 function display_difference(val1, val2)
-    if isnumeric(val1) && isnumeric(val2)
+    if isequal(size(val1), size(val2)) && isnumeric(val1) && isnumeric(val2)
         diffs = find(val1 ~= val2);
         if isempty(diffs)
             fprintf('No differences found.\n');
@@ -54,6 +54,6 @@ function display_difference(val1, val2)
         fprintf('Original: %s\n', val1);
         fprintf('Modified: %s\n', val2);
     else
-        fprintf('Original and modified values are of different types or cannot be displayed.\n');
+        fprintf('Original and modified values are of different sizes or types and cannot be compared directly.\n');
     end
 end
